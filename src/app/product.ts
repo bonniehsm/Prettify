bd33f9f015c942fa0c05420f45c8dae2b1a2ea61
bonnie.hung <bonnie.hung@accenture.com>
2019-11-08 17:19:14 -0500

    created Product class, mock-products, and products component

---
+++ src/app/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    id: number;
+    /* brand: string; */
+    category: string;
+    name: string;
+    price: number[];
+    size: string;
+    description: string;
+    images: string[];
+    options: any;  /* Other Shades, Product Sizes*/
+}
