627b42d2ace9543489ea918a83ecf8773e2eb6d7
bonnie.hung <bonnie.hung@hotmail.ca>
2019-11-21 12:11:14 -0500

    updated interface property

--- src/app/marked-item.ts
+++ src/app/marked-item.ts
@@ -1,6 +1,7 @@
 export interface MarkedItem {
     id: number;
     option: string;
+    price: number;
     addProduct(item: MarkedItem): void;
     removeProduct(item: MarkedItem): void;
 }
--- src/app/wishlist-item.ts
+++ src/app/wishlist-item.ts
@@ -3,9 +3,11 @@ import { MarkedItem } from './marked-item';
 class WishlistItem implements MarkedItem {
     id: number;
     option: string;
+    price: number;
     constructor(
         id: number,
         option: string,
+        price: number,
     ){}
     addProduct(item: MarkedItem) {
 
